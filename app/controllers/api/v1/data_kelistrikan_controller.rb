696ee9477b19ae79835417057fec3b8038941ea2
dzackyluc <dzackylucifer12345@gmail.com>
2024-06-25 06:34:17 +0700

    typo

--- app/controllers/api/v1/data_kelistrikan_controller.rb
+++ app/controllers/api/v1/data_kelistrikan_controller.rb
@@ -1,5 +1,5 @@
 module Api::V1
-    class DatakelistrikanController < ApplicationController
+    class DataKelistrikanController < ApplicationController
         skip_before_action :authorized, only: [:create, :update, :all_data_kelistrikan]
         rescue_from ActiveRecord::RecordInvalid, with: :Handle_invalid_record
 
